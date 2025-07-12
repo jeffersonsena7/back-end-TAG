e6886e551bf081d255c1e53f83f052e14c36aff6
Jefferson Sena <jefferson.senna777@gmail.com>
2025-07-12 11:54:12 -0300

    package.jason
    colocado o start do servidor

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(cors());
 app.use('/api/planilha', planilhaRouter);
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
