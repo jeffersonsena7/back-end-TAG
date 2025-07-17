8753714eab87a7d7afc3e6bfcf8ef1e87e0d100d
Jefferson Sena <jefferson.senna777@gmail.com>
2025-07-16 22:10:53 -0300

    adicionando uma coluna

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 // Monta as rotas prefixadas
 app.use('/api/planilha', planilhaRouter);
